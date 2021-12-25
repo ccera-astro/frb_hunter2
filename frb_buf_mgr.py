e8bca6387b6cbb368f23d41bf35495731d483f28
Marcus D. Leech <patchvonbraun@gmail.com>
2021-12-25 15:38:18 -0500

    Changed peak detection radically.

--- frb_buf_mgr.py
+++ frb_buf_mgr.py
@@ -9,70 +9,19 @@ import copy
 import shutil
 import json
 
+def ts_file(pacer,ts,prefix):
+    fn = prefix + "ts-" + "%d" % (int(ts)) + ".txt"
+    fp = open(fn, "w")
+    fp.write ("%.2f\n" % ts)
+    fp.close()
+    return True
+
 recently_triggered = 0
 events = []
-def cur_sidereal(longitude):
-    longstr = "%02d" % int(longitude)
-    longstr = longstr + ":"
-    longitude = abs(longitude)
-    frac = longitude - int(longitude)
-    frac *= 60
-    mins = int(frac)
-    longstr += "%02d" % mins
-    longstr += ":00"
-    x = ephem.Observer()
-    x.date = ephem.now()
-    x.long = longstr
-    jdate = ephem.julian_date(x)
-    tokens=str(x.sidereal_time()).split(":")
-    hours=int(tokens[0])
-    minutes=int(tokens[1])
-    seconds=int(float(tokens[2]))
-    sidt = "%02d,%02d,%02d" % (hours, minutes, seconds)
-    return (sidt)
 
-def save_frb_spectra(trig_val, fn,fftsize,longitude,prefix,decln,thresh,freq,bw,crate):
-    global recently_triggered
-    global events
-    if (trig_val > thresh):
-        if (recently_triggered <= 0):
-            evt = time.time()
-            ltp = time.gmtime(evt)
-            frac = evt-float(int(evt))
-            events.append(int(evt))
-            newfn = "frb-event-%d" % (int(evt))
-            sidt = cur_sidereal(longitude)
-            sidbits = sidt.split(",")
-            newfn += ".json"
-            evdict = {}
-            evdict["year"] = ltp.tm_year
-            evdict["month"] = ltp.tm_mon
-            evdict["day"] = ltp.tm_mday
-            evdict["hour"] = ltp.tm_hour
-            evdict["minute"] = ltp.tm_min
-            evdict["second"] = ltp.tm_sec
-            evdict["lmst_hour"] = int(sidbits[0])
-            evdict["lmst_minute"] = int(sidbits[1])
-            evdict["lmst_second"] = int(sidbits[2])
-            evdict["frequency"] = freq
-            evdict["bandwidth"] = bw
-            evdict["chan_rate"] = crate
-            evdict["declination"] = decln
-            evdict["fftsize"] = fftsize
-            evdict["threshold"] = thresh
-            fp = open (prefix+newfn, "w")
-            fp.write(json.dumps(evdict, indent=4))
-            fp.write("\n")
-            fp.close()
-            
-            #
-            # Prevent re-triggering immediately
-            #
-            recently_triggered = 240
-        else:
-            recently_triggered -= 1
-        
-    return True
+def add_event(event):
+	global events
+	events.append(event)
 
 def harvest(pacer,prefix,permdir):
     global events
